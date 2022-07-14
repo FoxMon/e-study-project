16293ad8bf4531cf484776ed5c1c3f010c6b5efd
foxmon <jjockrod@naver.com>
2022-07-14 19:24:09 +0900

    add router

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,17 @@
-import VideoListContainer from "./pages/videoListContainer"
-import Header from "./component/nav/header"
 import React from "react"
+import { Route, Routes } from "react-router-dom"
+import Header from "./component/nav/header"
+import VideoListContainer from "./pages/videoListContainer"
+import VideoDetail from "./component/videoDetail"
 
 function App() {
     return (
         <React.Fragment>
             <Header />
-            <VideoListContainer />
+            <Routes>
+                <Route path="/" element={<VideoListContainer />} />
+                <Route path="/detail" element={<VideoDetail />} />
+            </Routes>
         </React.Fragment>
     )
 }
