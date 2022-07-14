56ddf3139540fd552b8c3995be33704e0d9834db
foxmon <jjockrod@naver.com>
2022-07-14 20:21:03 +0900

    Refactor: videoDetail style

--- src/component/videoDetail.jsx
+++ src/component/videoDetail.jsx
@@ -1,17 +1,66 @@
+import React, { useState } from "react"
 import { useLocation } from "react-router"
-import { Box, CardMedia } from "@mui/material"
+import {
+    Box,
+    CardMedia,
+    CardActions,
+    styled,
+    IconButton,
+    Collapse,
+    CardContent,
+    Typography,
+} from "@mui/material"
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
+
+const ExpandMore = styled((props) => {
+    const { expand, ...other } = props
+    return <IconButton {...other} />
+})(({ theme, expand }) => ({
+    transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+    marginLeft: "auto",
+    color: "white",
+    transition: theme.transitions.create("transform", {
+        duration: theme.transitions.duration.shortest,
+    }),
+}))
 
 const VideoDetail = () => {
+    const [expanded, setExpanded] = useState(true)
+
     const { state } = useLocation()
+
+    const onExpandButtonClickEventHandler = () => {
+        setExpanded(!expanded)
+    }
+
     return (
-        <Box sx={boxStyle}>
-            <CardMedia
-                sx={mediaStyle}
-                component="iframe"
-                src={`https://www.youtube-nocookie.com/embed/${state.uuid}`}
-                alt="video"
-            />
-        </Box>
+        <React.Fragment>
+            <Box sx={boxStyle}>
+                <CardMedia
+                    sx={mediaStyle}
+                    component="iframe"
+                    src={`https://www.youtube-nocookie.com/embed/${state.uuid}`}
+                    alt="video"
+                />
+            </Box>
+            <Box sx={moreStyle}>
+                <CardActions disableSpacing>
+                    <ExpandMore
+                        expand={expanded}
+                        onClick={onExpandButtonClickEventHandler}
+                        aria-expanded={expanded}
+                    >
+                        <ExpandMoreIcon />
+                    </ExpandMore>
+                </CardActions>
+            </Box>
+            <Collapse sx={{ color: "white" }} in={expanded} unmountOnExit>
+                <CardContent>
+                    <Typography paragraph={true}>{state.title}</Typography>
+                    <Typography paragraph={true}>{state.channel}</Typography>
+                </CardContent>
+            </Collapse>
+        </React.Fragment>
     )
 }
 
@@ -23,6 +72,10 @@ const mediaStyle = {
     width: "90%",
     height: "500px",
     margin: "0 auto",
+    mt: 2,
+}
+
+const moreStyle = {
     mt: 2,
 }
 
