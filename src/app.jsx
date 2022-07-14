19c6de7c2bda0596e82aeea2329c64ae649dae06
foxmon <jjockrod@naver.com>
2022-07-14 19:43:46 +0900

    header 위치 수정

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,11 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
-import Header from "./component/nav/header"
 import VideoListContainer from "./pages/videoListContainer"
 import VideoDetail from "./component/videoDetail"
 
 function App() {
     return (
         <React.Fragment>
-            <Header />
             <Routes>
                 <Route path="/" element={<VideoListContainer />} />
                 <Route path="/detail" element={<VideoDetail />} />
