527b31911862e501e224155c181b6be56bcf7b1f
foxmon <jjockrod@naver.com>
2022-07-14 19:14:42 +0900

    add component

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,14 @@
 import VideoListContainer from "./pages/videoListContainer"
+import Header from "./component/nav/header"
+import React from "react"
 
 function App() {
-    return <VideoListContainer />
+    return (
+        <React.Fragment>
+            <Header />
+            <VideoListContainer />
+        </React.Fragment>
+    )
 }
 
 export default App
