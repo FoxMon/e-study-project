8330d86b58b35191ac65c762122b6049f6b69a8c
foxmon <jjockrod@naver.com>
2022-07-14 19:14:20 +0900

    header init

---
+++ src/component/nav/header.jsx
@@ -0,0 +1,115 @@
+import {
+    Box,
+    AppBar,
+    Toolbar,
+    IconButton,
+    Typography,
+    InputBase,
+    styled,
+    alpha,
+} from "@mui/material"
+import MenuIcon from "@mui/icons-material/Menu"
+import SearchIcon from "@mui/icons-material/Search"
+import { createTheme } from "@mui/material/styles"
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#d32f2f",
+            darker: "#053e85",
+        },
+    },
+})
+
+const Search = styled("div")(({ theme }) => ({
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    "&:hover": {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+        marginLeft: theme.spacing(1),
+        width: "auto",
+    },
+}))
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+}))
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: "inherit",
+    "& .MuiInputBase-input": {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create("width"),
+        width: "100%",
+        [theme.breakpoints.up("sm")]: {
+            width: "12ch",
+            "&:focus": {
+                width: "20ch",
+            },
+        },
+    },
+}))
+
+function Header() {
+    return (
+        <Box sx={boxStyle}>
+            <AppBar theme={theme} position="static">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        sx={buttonStyle}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="div"
+                        sx={contentStyle}
+                    >
+                        Metube
+                    </Typography>
+                    <Search>
+                        <SearchIconWrapper>
+                            <SearchIcon />
+                        </SearchIconWrapper>
+                        <StyledInputBase placeholder="Search…" />
+                    </Search>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    )
+}
+
+const boxStyle = {
+    flexGrow: 1,
+}
+
+const buttonStyle = {
+    mr: 2,
+}
+
+const contentStyle = {
+    flexGrow: 1,
+    display: {
+        xs: "none",
+        sm: "block",
+    },
+}
+
+export default Header
