5ffd912933763d2fb0955e25c3127e50b1c42853
foxmon <jjockrod@naver.com>
2022-07-14 19:24:20 +0900

    add navigation for detail

--- src/pages/videoListContainer.jsx
+++ src/pages/videoListContainer.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState, useCallback } from "react"
+import { useNavigate } from "react-router"
 import { Container } from "@mui/material"
 import Video from "../component/video"
-import VideoDetail from "../component/videoDetail"
 import YoutubeFetcher from "../fetcher/youtubeFetcher"
 
 const VideoListContainer = () => {
     const [videos, setVideos] = useState([])
     const [selected, setSelected] = useState(null)
+    const navigate = useNavigate()
 
     useEffect(() => {
         YoutubeFetcher.getMostPopular().then((result) => {
@@ -16,11 +17,13 @@ const VideoListContainer = () => {
 
     const onPlayButtonClickEventHandler = useCallback((video) => {
         setSelected(video)
+        navigate("/detail", {
+            state: video,
+        })
     }, [])
 
     return (
         <Container sx={containerStyle}>
-            {selected && <VideoDetail selectedVideo={selected} />}
             {videos.map((v) => (
                 <Video
                     key={v.id}
